635e9ebeead5f83a32104fa4521b35927799540e
魔法少女小圆 <540193114@qq.om>
2024-08-04 23:07:35 +0800

    feat: detail

--- entry/detail/detail.js
+++ entry/detail/detail.js
@@ -1,6 +1,11 @@
 
 Page({
   data: {
+    goods: {
+      tags: ['闪电', '旋风'],
+      picUrl: 'https://img14.360buyimg.com/n1/jfs/t1/60990/38/26374/53586/66ac4fe4Fccc94b38/ac5603909959dd7f.jpg',
+      goodsName: 'Apple AirPods Max 苹果耳机头戴式 max无线蓝牙耳机耳麦 天蓝色 标配'
+    }
   },
 
   onLoad() {
